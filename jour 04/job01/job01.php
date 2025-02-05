e9b3a668b97100fa0b7db07c6aebc551ee0e49da
Réda YAHYAOUI <yahyaouir@yahoo.frgit config --global user.name Reda21-eng>
2025-02-05 11:49:49 +0100

    fonctions

--- jour 04/job01/job01.php
+++ jour 04/job01/job01.php
@@ -1,9 +1,13 @@
-<php
-class Personne
-{
-public $age =14;
+<?php
+class Personne {
+    public $age = 14;
 
-afficher age ();
-echo($age) 14;
+    public function afficherAge() {
+        echo($this->age);
+    }
+
+    public function bonjour() {
+        // code pour  afficher en console
+    }
 }
 ?>
